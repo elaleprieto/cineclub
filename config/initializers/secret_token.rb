84ae79c957afa1f06711c09cb9563e7011d488d2
Ale Prieto <aleprieto@gmail.com>
2013-07-11 12:07:29 -0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cineclub::Application.config.secret_token = 'bc8a8ac5d740ce7d266aabf0c574eed1ec465053aef9b16c1d4a5dbffd47b6552422a16c710d044347ed372d5b7fb8d833b9394a360433a6134084b29b23d55f'
